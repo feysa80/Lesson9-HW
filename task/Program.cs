4cddc55f71d5bc9a22553373b1aab82a778a7472
SvetaF <feyginas@gmail.com>
2022-10-20 19:30:13 +0300

    Добавила решение 3-й задачи

--- task/Program.cs
+++ task/Program.cs
@@ -2,10 +2,12 @@
 //в промежутке от M до N с помощью рекурсии.
 // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
 // в промежутке от M до N с помощью рекурсии.
+//Задача 68: Задайте значения M и N. Напишите программу, которая найдёт наибольший общий делитель (НОД) этих чисел с помощью рекурсии.
 
 
 string task64 = "Задача 64: Задайте значения N и M. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.";
 string task66 = "Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.";
+string task68 = "Задача 68: Задайте значения M и N. Напишите программу, которая найдёт наибольший общий делитель (НОД) этих чисел с помощью рекурсии.";
 Console.Write("Введите номер задачи: ");
 int task = Convert.ToInt32(Console.ReadLine());
 switch (task)
@@ -23,7 +25,13 @@ switch (task)
         int result = GetSum(m66,n66,m66);
         Console.WriteLine($"Сумма чисел от {m66} до {n66} равна {result}");
         break;
-
+    case 68:
+        var data68 = CheckData2(task68);
+        int m68 = Convert.ToInt32(data68.newStart);
+        int n68 = Convert.ToInt32(data68.newEnd);
+        int result68 = MaxDevide(m68,n68, n68);
+        Console.WriteLine($"Наибольший общий делитель для чисел {m68} и {n68} - {result68}");
+        break;
     default:
         Console.WriteLine("Неправильный ввод");
         break;
@@ -67,6 +75,17 @@ void Task64(int start, int end)
         return (start1,end1);
     } else return (start1,end1);
 }
+(int newStart,int newEnd) CheckData2(string task){
+    var dataTask = Begin(task);
+    int start1 = Convert.ToInt32(dataTask.m);
+    int end1 = Convert.ToInt32(dataTask.n);
+    if(start1 < end1){
+        int temp = end1;
+        end1 = start1;
+        start1 = end1;
+        return (start1,end1);
+    } else return (start1,end1);
+}
 int GetSum(int start, int end, int sum)
 {   if (start < end)
     {
@@ -76,4 +95,14 @@ int GetSum(int start, int end, int sum)
         
     } else  return sum;
     
+}
+int MaxDevide(int first, int second, int second2){
+    
+   if(first == second) return second;
+   if(first % second2 == 0 && second % second2 ==0) return second2;
+   else{
+    second2 = second2 -1;
+    return MaxDevide(first, second, second2);
+   }
+
 }
